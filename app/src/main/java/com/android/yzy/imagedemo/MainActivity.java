ebeef5f97849b6e95a4bebf011b0f29bdaa5e6fa
yanzy <1187997355@qq.com>
2018-08-30 09:44:25 +0800

    adjustViewBounds

--- app/src/main/java/com/android/yzy/imagedemo/MainActivity.java
+++ app/src/main/java/com/android/yzy/imagedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.yzy.imagedemo;
 
+import android.graphics.drawable.ScaleDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.iv_image);
     }
 }
